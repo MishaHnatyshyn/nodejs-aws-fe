37fc70362d934fdfb9a8ca49800b9d7579f552b6
MishaHnatyshyn <gantyshynmisha@gmail.com>
2020-11-16 14:43:11 +0200

    update import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://e0y68fypfe.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://lcy7fs8of3.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
