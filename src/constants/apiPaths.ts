6e05cb29eefe55f145f30e8d3ce8344dee19fb75
MishaHnatyshyn <gantyshynmisha@gmail.com>
2020-12-06 13:54:28 +0200

    update

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://e0y68fypfe.execute-api.us-east-1.amazonaws.com/dev',
+  product: 'https://7ijl3bf0w3.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://lcy7fs8of3.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://pspm0wkluk.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
