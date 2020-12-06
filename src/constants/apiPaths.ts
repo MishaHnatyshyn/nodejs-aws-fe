e3578de02553dad290e12b3b8e82ce3aaad6ec88
MishaHnatyshyn <gantyshynmisha@gmail.com>
2020-12-06 14:05:19 +0200

    Connect to cart API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://7ijl3bf0w3.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://mishahnatyshyn-cart-api-develop.us-east-1.elasticbeanstalk.com/',
   import: 'https://pspm0wkluk.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
